79660590caffca5dc3c34630d22698f7dab40f83
Henrique Fernandes <henrique.fquick@gmail.com>
2019-11-16 23:52:06 -0300

    Adicionados classe FICHA e update no layout

--- app/src/main/java/com/example/app_fichas_rpg/MainActivity.java
+++ app/src/main/java/com/example/app_fichas_rpg/MainActivity.java
@@ -2,13 +2,26 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+     public class MainActivity extends AppCompatActivity {
 
+    public Button btngo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btngo = findViewById(R.id.btn_go);
+
+        btngo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CriarFichaActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
