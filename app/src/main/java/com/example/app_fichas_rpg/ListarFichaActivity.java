bfd404147eb9d10b9a3e18f2aace35d6caa4b1ad
Henrique Fernandes <henrique.fquick@gmail.com>
2019-11-25 12:15:35 -0300

    Arquivos de listarfichas

---
+++ app/src/main/java/com/example/app_fichas_rpg/ListarFichaActivity.java
@@ -0,0 +1,34 @@
+package com.example.app_fichas_rpg;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.app_fichas_rpg.adapters.FichaAdapter;
+
+import java.util.List;
+
+public class ListarFichaActivity extends AppCompatActivity {
+
+    public List<Ficha> fichas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_ficha);
+
+        DataBase db = new DataBase();
+
+        fichas = db.Listar();
+
+        RecyclerView recyclerView = findViewById(R.id.fichas_lista);
+        FichaAdapter adapter = new FichaAdapter(fichas, getApplicationContext());
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getApplicationContext()));
+    }
+}
---
+++ app/src/main/java/com/example/app_fichas_rpg/adapters/FichaAdapter.java
@@ -0,0 +1,91 @@
+package com.example.app_fichas_rpg.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.app_fichas_rpg.Ficha;
+import com.example.app_fichas_rpg.R;
+
+import java.util.List;
+
+public class FichaAdapter extends RecyclerView.Adapter<FichaAdapter.ViewHolder> {
+
+    private List<Ficha> fichas;
+
+    private Context mContext;
+
+
+    public FichaAdapter(List<Ficha> fichas, Context mContext) {
+
+        this.fichas = fichas;
+
+        this.mContext = mContext;
+
+    }
+
+
+    @NonNull
+
+    @Override
+
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_ficha, parent, false);
+
+        return new ViewHolder(view);
+
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        holder.name.setText(fichas.get(position).getNome());
+        holder.nivel.setText(fichas.get(position).getNivel());
+
+        holder.container.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+
+            }
+
+        });
+
+    }
+
+
+    @Override
+
+    public int getItemCount() {
+
+        return fichas.size();
+
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView name;
+        TextView nivel;
+        LinearLayout container;
+
+        public ViewHolder(@NonNull View itemView) {
+
+            super(itemView);
+
+            name = itemView.findViewById(R.id.txt_Personagem_Ficha);
+            nivel = itemView.findViewById(R.id.txt_Nivel_Ficha);
+            container = itemView.findViewById(R.id.container);
+
+        }
+
+    }
+}
